af5815fdc423be9952b516f6c33aedd343bf4001
botp <botp@5d8f152a-1e75-4404-8c64-3cb1646ade7d>
2012-03-26 11:11:22 +0000

    deleted erroneous directory /branches/trunk -Bo

--- branches/trunk/src/functions/FactorPreconditioner.f90
+++
@@ -1,8 +0,0 @@
-SUBROUTINE FactorPreconditioner(PreconditioningMatrix, Nfs)
-USE Precision
-USE DataTypes
-IMPLICIT NONE
-INTEGER :: Nfs
-TYPE (SparseArray_COO) :: PreconditioningMatrix
-CALL LUfactor(PreconditioningMatrix, Nfs)
-END SUBROUTINE FactorPreconditioner
