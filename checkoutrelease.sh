4712a9b491757db581635ea4cb42c15dc757e835
apek <apek@5d8f152a-1e75-4404-8c64-3cb1646ade7d>
2014-04-28 19:13:45 +0000

    how to release setup

--- checkoutrelease.sh
+++ checkoutrelease.sh
@@ -3,5 +3,8 @@ PRODUCT="OceanWave3D"
 USER=apek
 NUM=0.99.0
 DIR="$PRODUCT"
-svn export svn://$USER@svn.gbar.dtu.dk/$USER/OceanWave3D/tags/Release-$NUM
-tar czf$PRODUCT-$NUM.tar.gz $DIR
+svn export svn://$USER@svn.gbar.dtu.dk/$USER/OceanWave3D/tags/Release-$NUM ../$DIR-$NUM
+tar czf $PRODUCT-$NUM.tar.gz ../$DIR-$NUM
+mv $PRODUCT-$NUM.tar.gz ../.
+
+
