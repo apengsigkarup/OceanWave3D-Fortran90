6379866cb13cd2a4f1965ea5aaa8deb877162430
botp <botp@5d8f152a-1e75-4404-8c64-3cb1646ade7d>
2015-07-09 11:09:03 +0000

    Missing files added

---
+++ branches/botp/src/wrappers/LUfactor_spk.f90
@@ -0,0 +1,33 @@
+SUBROUTINE LUfactor_spk(SparseMatrix, Nr)
+!
+! LU factor matrix given in sparse format.
+!
+! By Allan P. Engsig-Karup.
+USE Precision
+USE Constants
+USE DataTypes
+USE SPK
+USE GlobalVariables, ONLY: GMRESmaxiterations
+IMPLICIT NONE
+TYPE (SparseArray_COO) :: SparseMatrix
+INTEGER :: Nr, Nxz, nnz, STAT,i,j
+
+REAL(8) :: droptol
+INTEGER :: lfil, IWK,workspaceSize
+
+nnz   = SIZE(SparseMatrix%val)
+nxz   = Nr ! total number of grid points
+
+
+lfil = 5
+IWK  = nnz + lfil + 1 
+droptol = 0.01
+workspaceSize =(Nr+3)*(GMRESmaxiterations+2) + (GMRESmaxiterations+1) * GMRESmaxiterations/2
+
+ALLOCATE(alu(IWK),jlu(IWK),ju(Nr),w(workspaceSize),jw(3*Nr))
+print *, 'workspaceSize = ', workspaceSize
+
+
+CALL ILUT(Nr,SparseMatrix%val,SparseMatrix%col_ind,SparseMatrix%row_ptr,lfil,droptol,alu,jlu,ju,IWK,w,jw,ierr)
+
+END SUBROUTINE LUfactor_spk
