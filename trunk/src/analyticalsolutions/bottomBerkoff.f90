ddb0122480f556d05e4ba4ce82552c4c3422bddb
apek <apek@5d8f152a-1e75-4404-8c64-3cb1646ade7d>
2015-08-22 19:38:20 +0000

    added berkhoff setup

---
+++ trunk/src/analyticalsolutions/bottomBerkoff.f90
@@ -0,0 +1,64 @@
+SUBROUTINE BottomBerkoff(FineGrid)
+!
+! Define bottom profile for Berkhoff experiment (3D)
+!
+! By Allan P. Engsig-Karup.
+USE Precision
+USE Constants
+USE DataTypes
+IMPLICIT NONE
+TYPE (Level_def) :: FineGrid
+REAL(KIND=long) :: y, x, xr, yr, k, angle, dxrdx, dxrdy, dyrdx, dyrdy
+INTEGER :: Nx, Ny, i, j
+Nx = FineGrid%Nx
+Ny = FineGrid%Ny
+angle = 20
+k = angle/180.0_long*pi
+DO j = 1, Ny
+   y = FineGrid%y(j) - 11.0_long ! FIXME: translation relative to standard y-coordinates
+   DO i = 1 , Nx
+      x = FineGrid%x(i) - half*FineGrid%x(Nx) ! position shoal in middle relative to y-axis
+      xr = cos(k)*x - sin(k)*y
+      yr = sin(k)*x + cos(k)*y
+      dxrdx = cos(k)
+      dyrdx = sin(k)
+      dxrdy = -sin(k)
+      dyrdy = cos(k)
+      IF (yr<=-5.82_long) THEN
+         FineGrid%h(i,j)   = 0.45_long
+         FineGrid%hx(i,j)  = zero
+         FineGrid%hy(i,j)  = zero
+         FineGrid%hxx(i,j) = zero
+         FineGrid%hyy(i,j) = zero
+      ELSE ! IF (yr>-5.82_long) THEN
+         FineGrid%h(i,j)   = 0.45_long-0.02_long*(5.82_long+yr)
+         FineGrid%hx(i,j)  = -0.02_long*dyrdx
+         FineGrid%hy(i,j)  = -0.02_long*dyrdy
+         FineGrid%hxx(i,j) = zero
+         FineGrid%hyy(i,j) = zero
+      END IF
+      IF ((xr/four)**2 + (yr/three)**2<1) THEN
+         FineGrid%h(i,j)   = FineGrid%h(i,j) + 0.3_long - half*sqrt(one-(xr/five)**2-(yr/3.75_long)**2)
+!WRITE(*,*) '  WARNING: The bottom slopes for the Berkhoff experiment is not defined analytically (i.e. not implemented).'
+!WRITE(*,*) '  WARNING: Therefore the derivatives of the bottom profile function is determined numerically.'
+         FineGrid%hx(i,j)  = FineGrid%hx(i,j)  + (0.02_long*xr*dxrdx + &
+             0.0355556_long*yr*dyrdx)/sqrt(one-0.04_long*xr**2-0.0711111_long*yr**2) 
+         FineGrid%hy(i,j)  = FineGrid%hy(i,j)  + (0.02_long*xr*dxrdy + & 
+             0.0355556_long*yr*dyrdy)/sqrt(one-0.04_long*xr**2-0.0711111_long*yr**2) 
+!         FineGrid%hxx(i,j) = FineGrid%hxx(i,j) + 
+!         FineGrid%hyy(i,j) = FineGrid%hyy(i,j) + 
+      END IF
+      IF (FineGrid%h(i,j)<0.1_long) THEN
+         ! flat bottom profile after shoal 
+         ! - this is very absorption zone should effectively damp outgoing waves
+         FineGrid%h(i,j)   = 0.1_long
+         FineGrid%hx(i,j)  = zero
+         FineGrid%hy(i,j)  = zero
+         FineGrid%hxx(i,j) = zero
+         FineGrid%hyy(i,j) = zero
+      END IF
+   END DO
+END DO
+WRITE(*,*) '  WARNING: The bottom slopes for the Berkhoff experiment is not defined analytically (i.e. not implemented).'
+WRITE(*,*) '  WARNING: Therefore the derivatives of the bottom profile function is determined numerically.'
+END SUBROUTINE BottomBerkoff
